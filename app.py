dfbd716f33dcc6961b23b6146680f264835e121d
EcoG-one <ecog@outlook.de>
2025-03-06 04:03:25 +0100

    add content created

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import json
 
 app = Flask(__name__)
+file_path = 'blogs.json'
 
+def load_posts():
+    with open(file_path, "r") as json_file:
+        blog_posts = json.loads(json_file.read())
+    json_file.close()
+    return blog_posts
+
+def save_posts(posts):
+    with open(file_path, "w") as json_file:
+        json.dump(posts, json_file)
+    json_file.close()
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', posts=load_posts())
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
+        blog_posts = load_posts()
+        id = blog_posts[-1]["id"] + 1
+        blog_posts.append({"id": id, "author": author, "title": title, "content": content})
+        save_posts(blog_posts)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
